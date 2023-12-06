78e9081cf5209e052bc2978e17e0925c33b93789
Vu Ngoc Hieu <hieu.vn@extremevn.com.vn>
2023-12-06 09:41:04 +0700

    Setup project, create navigation

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+import {RouterView} from 'vue-router'
+import SiteNavigation from "./components/SiteNavigation.vue";
+</script>
+
+<style scoped></style>
